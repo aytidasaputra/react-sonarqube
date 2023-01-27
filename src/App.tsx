434cf2aee0f196c6b478d225b39e3e8a899f2c1d
ddx <aditya.saputra@brilife.co.id>
2023-01-27 09:18:50 +0700

    try unit test with jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+const incrementCounter = () => {
+setCounter((prevCounter) => prevCounter + 1);
+};
+
+const decrementCounter = () => {
+setCounter((prevCounter) => prevCounter - 1);
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button data-testid="increment" onClick={incrementCounter}>
+      +
+      </button>
+      <p data-testid="counter">{counter}</p>
+      <button disabled data-testid="decrement" onClick={decrementCounter}>
+      -
+      </button>
     </div>
   );
 }
